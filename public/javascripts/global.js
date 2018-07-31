195c9f63280690f4878df50388d024eeccd1c411
ivclexp <40245350+ivclexp@users.noreply.github.com>
2018-07-31 16:06:01 -0400

    Update global.js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 	});
 
 	//2. draw correlation
-	d3.csv('public/data/Scimem Dataset.csv',function(error,data){
-		drawCorrelation(data,'distinct colors','score');
+	d3.csv('public/data/Scimem Dataset.csv', function (error, data) {
+		drawCorrelation(data, 'Number of distinct colors', 'score');
 	});
 
 	//3. blind event on ratio change
@@ -18,7 +18,7 @@ $(document).ready(function () {
 		var xValue = $('#x-select').val();
 		var yValue = $('#y-select').val();
 		var filename = $("input[name='datasetratio']:checked").val();
-		var filepath = 'public/data/' + filename +'.csv';
+		var filepath = 'public/data/' + filename + '.csv';
 		d3.csv(filepath, function (error, data) {
 			drawCorrelation(data, xValue, yValue);
 		});
@@ -28,7 +28,7 @@ $(document).ready(function () {
 		var xValue = $('#x-select').val();
 		var yValue = $('#y-select').val();
 		var filename = $("input[name='datasetratio']:checked").val();
-		var filepath = 'public/data/' + filename +'.csv';
+		var filepath = 'public/data/' + filename + '.csv';
 		d3.csv(filepath, function (error, data) {
 			drawCorrelation(data, xValue, yValue);
 		});
@@ -39,7 +39,7 @@ $(document).ready(function () {
 		var xValue = $('#mx-select').val();
 		var yValue = $('#my-select').val();
 		var filename = $("input[name='datasetratio']:checked").val();
-		var filepath = 'public/data/' + filename +'.csv';
+		var filepath = 'public/data/' + filename + '.csv';
 		d3.csv(filepath, function (error, data) {
 			drawCorrelation(data, xValue, yValue);
 		});
@@ -49,29 +49,29 @@ $(document).ready(function () {
 		var xValue = $('#mx-select').val();
 		var yValue = $('#my-select').val();
 		var filename = $("input[name='datasetratio']:checked").val();
-		var filepath = 'public/data/' + filename +'.csv';
+		var filepath = 'public/data/' + filename + '.csv';
 		d3.csv(filepath, function (error, data) {
 			drawCorrelation(data, xValue, yValue);
 		});
 	})
 
-	$('input[type=radio][name=datasetratio]').change(function() {
-		if(this.value == 'Scimem Dataset'){
-			$('#mx-select').css('display','none');
-			$('#my-select').css('display','none');
-			$('#x-select').css('display','block');
-			$('#y-select').css('display','block');
+	$('input[type=radio][name=datasetratio]').change(function () {
+		if (this.value == 'Scimem Dataset') {
+			$('#mx-select').css('display', 'none');
+			$('#my-select').css('display', 'none');
+			$('#x-select').css('display', 'block');
+			$('#y-select').css('display', 'block');
 			var xValue = $('#x-select').val();
 			var yValue = $('#y-select').val();
 			d3.csv('public/data/Scimem Dataset.csv', function (error, data) {
 				drawCorrelation(data, xValue, yValue);
 			});
 		}
-		else if(this.value == 'Massive Dataset'){
-			$('#mx-select').css('display','block');
-			$('#my-select').css('display','block');
-			$('#x-select').css('display','none');
-			$('#y-select').css('display','none');
+		else if (this.value == 'Massive Dataset') {
+			$('#mx-select').css('display', 'block');
+			$('#my-select').css('display', 'block');
+			$('#x-select').css('display', 'none');
+			$('#y-select').css('display', 'none');
 			var xValue = $('#mx-select').val();
 			var yValue = $('#my-select').val();
 			d3.csv('public/data/Massive Dataset.csv', function (error, data) {
@@ -91,139 +91,167 @@ function showVisualization(data) {
 		var img_score = data[i].score;
 		var outer_div = document.createElement("div");
 		outer_div.className = "col-md-2 image-div";
-		outer_div.innerHTML = '<div class="card image-grid"><div class="panel-thumbnail img-panel"><a target="blank" href='+img_url+' class="d-block mb-3  image-a"><img class="img-fluid img-thumbnial" src=' + img_thumburl + ' alt=""></a></div><div class="card-body" id="score-panel"><p class="score-text">score: ' + img_score + '</p></div></div>';
+		outer_div.innerHTML = '<div class="card image-grid"><div class="panel-thumbnail img-panel"><a target="blank" href=' + img_url + ' class="d-block mb-3  image-a"><img class="img-fluid img-thumbnial" src=' + img_thumburl + ' alt=""></a></div><div class="card-body" id="score-panel"><p class="score-text">score: ' + img_score + '</p></div></div>';
 		document.getElementById("image-gallery").appendChild(outer_div);
 
 	}
 }
 
-function drawCorrelation(data,xLabel,yLabel){
+function drawCorrelation(data, xLabel, yLabel) {
 
 	d3.select("#svgcorr").remove();
 
-	var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 320 - margin.left - margin.right,
-    height = 320 - margin.top - margin.bottom;
+	var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+		width = 320 - margin.left - margin.right,
+		height = 320 - margin.top - margin.bottom;
 	var xArray = [];
 	var yArray = [];
 
-	data.forEach(function(d) {
+	data.forEach(function (d) {
 		xArray.push(d[xLabel]);
 		yArray.push(d[yLabel]);
-      	d.x = parseFloat(d[xLabel]);
-      	d.y = parseFloat(d[yLabel]);
-      	d.h = parseFloat(d.meanH);
-      	d.s = parseFloat(d.meanS);
+		d.x = parseFloat(d[xLabel]);
+		d.y = parseFloat(d[yLabel]);
+		d.h = parseFloat(d.meanH);
+		d.s = parseFloat(d.meanS);
 		d.v = parseFloat(d.meanV);
 		d.L = parseFloat(d.L);
 		// d.A = parseFloat(d.A);
 		// d.B = parseFloat(d.B);
 		d.C = parseFloat(d.C);
 		d.H = parseFloat(d.H);
-      	d.url = d.url;
-  	});
+		d.url = d.url;
+	});
 
 	//scale
-	var xLowest = d3.min(data,function(d){
+	var xLowest = d3.min(data, function (d) {
 		return 0;
 	})
-	var xHighest = d3.max(data,function(d){
+	var xHighest = d3.max(data, function (d) {
 		return d.x;
 	})
-	var yLowest = d3.min(data,function(d){
+	var yLowest = d3.min(data, function (d) {
 		return 0;
 	})
-	var yHighest = d3.max(data,function(d){
+	var yHighest = d3.max(data, function (d) {
 		return d.y;
 	})
 
 	//spearman rank
-	corr = spearson.correlation.spearman(xArray,yArray).toFixed(3);
+	corr = spearson.correlation.spearman(xArray, yArray).toFixed(3);
 
 	$('#correlation').text(corr);
 
+	
 	var x = d3.scaleLinear()
-		.domain([xLowest,xHighest])
-		.range([0,width])
+		.domain([xLowest, xHighest])
+		.range([0, width])
 
 	var y = d3.scaleLinear()
-		.domain([yLowest,yHighest])
-		.range([height,0])
+		.domain([yLowest, yHighest])
+		.range([height, 0])
 
- 	var plot = "corr";
+	var plot = "corr";
 
 	var svg = d3.select('#corre-result-box').append("svg")
-		.attr("id",function(d,i){
-			return "svg"+plot;
+		.attr("id", function (d, i) {
+			return "svg" + plot;
 		})
-		.attr("class","svg")
-		.attr("width",380)
-		.attr("height",400)
+		.attr("class", "svg")
+		.attr("width", 380)
+		.attr("height", 400)
 		.append("g")
-    	.attr("transform", "translate(" + 80 + "," + margin.top + ")");
+		.attr("transform", "translate(" + 80 + "," + margin.top + ")");
 
-    var xAxis = d3.axisBottom(x);
-    var yAxis = d3.axisLeft(y)
+	var formatxAxis = d3.format('.0f');
+	var xTickNumber, yTickNumber;
 
-    svg.append("g")
+	if(yHighest < 4){
+		yTickNumber = 3;
+	}
+	else if(4 < yHighest <= 7){
+		yTickNumber = 7;
+	}
+	else if(7 < yHighest < 8){
+		yTickNumber = 7;
+	}
+
+	if(xHighest < 4){
+		xTickNumber = 3;
+	}
+	else if(4 < xHighest <= 7){
+		xTickNumber = 7;
+	}
+	else if(7 < xHighest < 8){
+		xTickNumber = 7;
+	}
+
+	var xAxis = d3.axisBottom(x)
+		.ticks(xTickNumber);
+	var yAxis = d3.axisLeft(y)
+		.ticks(yTickNumber);
+	//.tickFormat(formatxAxis);
+	svg.append("g")
 		.attr("transform", "translate(0,280)")
-      	.call(xAxis)
-      .append("text")
-      .style("font-size","1rem")
-      .attr("fill", "#000")
-      .attr("x",160)
-      .attr("y", 25)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text(xLabel);
+		.call(xAxis)
+		.append("text")
+		.style("font-size", "1rem")
+		.attr("fill", "#000")
+		.attr("x", 160)
+		.attr("y", 25)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text(xLabel);
 
-    svg.append("g")
+	svg.append("g")
 		.attr("transform", "translate(0,0)")
-      	.call(yAxis)
-      .append("text")
-      .style("font-size","1rem")
-      .style("font-family","Arial")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("x",-100)
-      .attr("y", -50)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text(yLabel);
+		.call(yAxis)
+		.append("text")
+		.style("font-size", "1rem")
+		.style("font-family", "Arial")
+		.attr("fill", "#000")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -100)
+		.attr("y", -50)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text(yLabel);
 
 
-    var points = svg.selectAll('.dot')
-	.data(data)
-	.enter()
-	.append('circle')
-	.attr('cx', function(d,i) {
-		return x(d.x);
-	})	
-	.attr('cy', function(d) {
-		return y(d.y);
-	})	
-	.on("mouseover",function(d,i){
-		//console.log(d);
-        d3.select(this).attr("r",5);
-        d3.select(this).style("opacity","0.9");
-        d3.select(this).style("cursor", "pointer"); 
-        $('.origin-img').attr("src",d.url);
-    })
-    .on("mouseout",function(){
-        d3.select(this).attr("r",5);
-        d3.select(this).style("opacity","0.9");
-        d3.select(this).style("cursor", "default"); 
+	var points = svg.selectAll('.dot')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('cx', function (d, i) {
+			return x(d.x);
+		})
+		.attr('cy', function (d) {
+			return y(d.y);
+		})
+		.on("mouseover", function (d, i) {
+			//console.log(d);
+			d3.select(this).attr("r", 7);
+			d3.select(this).style("opacity", "0.9");
+			d3.select(this).style("cursor", "pointer");
+			d3.select(this).style("fill","#f1c40f");
+			$('.origin-img').attr("src", d.url);
+		})
+		.on("mouseout", function () {
+			d3.select(this).attr("r", 5);
+			d3.select(this).style("opacity", "0.9");
+			d3.select(this).style("fill","#4684b1");
+			d3.select(this).style("cursor", "default");
 
-    })
-	.style("fill",function(d,i){
-		//return d3.lch(d.L,d.C,d.H);
-		//return d3.lab(d.L,d.A,d.B);
-		//return d3.hsv(d.h*360,d.s,d.v);
-		return '#4684b1';
+		})
+		.style("fill", function (d, i) {
+			//return d3.lch(d.L,d.C,d.H);
+			//return d3.lab(d.L,d.A,d.B);
+			//return d3.hsv(d.h*360,d.s,d.v);
+			return '#4684b1';
 
-	})
-	.style("opacity","0.8")
-	.attr('r', 5);
+		})
+		.style("opacity", "0.8")
+		.attr('r', 5);
 
 }
 
